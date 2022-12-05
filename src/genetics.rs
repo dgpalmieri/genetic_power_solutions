333181b2f527ce0c4fa68c18011ed50eb1142960
Dylan Palmieri <dgpalmieri@gmail.com>
2022-12-04 15:48:30 -0900

    feat: add selection

--- src/genetics.rs
+++ src/genetics.rs
@@ -5,8 +5,10 @@
 mod chromosome;
 use chromosome::Chromosome;
 use csv::Reader;
+use rand::Rng;
 
 use std::collections::HashMap;
+use std::f64::INFINITY;
 use std::fs::read_dir;
 use std::io::Error;
 use std::path::Path;
@@ -29,25 +31,20 @@ impl Genetics {
     }
 
     pub fn calculate_dataset_fitness(&mut self, data_dir: &Path) -> Result<(), Error> {
-        let mut data_files: Vec<std::path::PathBuf> = Vec::new();
+        let mut data: HashMap<std::path::PathBuf, Vec<f32>> = HashMap::new();
         for f in read_dir(data_dir)? {
-            let path = f?.path();
-            if path.exists() && !path.is_dir() {
-                data_files.push(path)
+            let data_file = f?.path();
+            if data_file.exists() && !data_file.is_dir() {
+                let mut rdr = Reader::from_path(&data_file)?;
+                let mut temp_data: Vec<f32> = Vec::new();
+
+                while let Some(result) = rdr.records().next() {
+                    let record = result?;
+                    temp_data.push(record[1].parse().unwrap());
+                }
+
+                data.insert(data_file, temp_data);
             }
-        }
-
-        let mut data: HashMap<&std::path::PathBuf, Vec<f32>> = HashMap::new();
-        for df in data_files.iter() {
-            let mut rdr = Reader::from_path(df)?;
-            let mut temp_data: Vec<f32> = Vec::new();
-
-            while let Some(result) = rdr.records().next() {
-                let record = result?;
-                temp_data.push(record[1].parse().unwrap());
-            }
-
-            data.insert(df, temp_data.clone());
         }
 
         for c in self.population.iter_mut() {
@@ -60,4 +57,46 @@ impl Genetics {
 
         Ok(())
     }
+
+    pub fn selection(&mut self, selection_rates: (i8, i8, i8)) -> () {
+        assert!(selection_rates.0 + selection_rates.1 + selection_rates.2 == 100);
+        let random_selection = selection_rates.0 / self.population.len() as i8;
+        let tournament_selection = selection_rates.1 / self.population.len() as i8;
+        let generate_new_selection = selection_rates.2 / self.population.len() as i8;
+
+        let mut new_pop: Vec<Chromosome> = Vec::new();
+
+        for _ in 0..random_selection {
+            let i = rand::thread_rng().gen_range(0..self.population.len());
+            new_pop.push(self.population.swap_remove(i));
+        }
+
+        let tourney_size = self.population.len() / 10;
+        for _ in 0..tournament_selection {
+            let initial_index =
+                rand::thread_rng().gen_range(0..self.population.len() - tourney_size);
+            let tourney_pop = self.population[initial_index..initial_index + tourney_size].to_vec();
+
+            let mut winner_index = initial_index;
+            let mut min_fitness = INFINITY;
+            let mut slice_index = 0;
+            for c in tourney_pop.iter() {
+                if (c.fitness as f64) < min_fitness {
+                    min_fitness = c.fitness as f64;
+                    winner_index = initial_index + slice_index;
+                }
+                slice_index += 1;
+            }
+
+            new_pop.push(self.population.swap_remove(winner_index));
+        }
+
+        for _ in 0..generate_new_selection {
+            let i = rand::thread_rng().gen_range(0..self.population.len());
+            new_pop.insert(i, Chromosome::new());
+        }
+    }
+
+    pub fn crossover(&self) -> () {}
+    pub fn mutation(&self) -> () {}
 }
