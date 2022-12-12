716696688ac2d84d0658bd9d4fa704f37122b012
Dylan Palmieri <dgpalmieri@gmail.com>
2022-12-11 17:49:03 -0900

    style: decrease max_line_length to 88

--- src/genetics.rs
+++ src/genetics.rs
@@ -25,11 +25,16 @@ impl Genetics {
         self.population.iter()
     }
 
-    pub fn set_fitness_for_dataset(&mut self, data: &HashMap<std::path::PathBuf, Vec<f32>>) -> () {
+    pub fn set_fitness_for_dataset(
+        &mut self,
+        data: &HashMap<std::path::PathBuf, Vec<f32>>,
+        generate_csv: &bool,
+    ) -> () {
         for c in self.population.iter_mut() {
             let mut fitness_sum: f32 = 0.0;
-            for (_, d) in data.iter() {
-                fitness_sum += c.calculate_sample_fitness(&d);
+            for (f, d) in data.iter() {
+                fitness_sum +=
+                    c.calculate_sample_fitness(&f.to_str().unwrap(), &d, generate_csv);
             }
             c.fitness = fitness_sum / data.len() as f32;
         }
@@ -38,9 +43,12 @@ impl Genetics {
     pub fn selection(&mut self, selection_rates: &(i8, i8, i8)) -> () {
         assert!(selection_rates.0 + selection_rates.1 + selection_rates.2 == 100);
         let population_modifier: f32 = 1.0 / 100.0 * self.population.len() as f32;
-        let mut random_selection: i16 = (selection_rates.0 as f32 * population_modifier) as i16;
-        let tournament_selection: i16 = (selection_rates.1 as f32 * population_modifier) as i16;
-        let generate_new_selection: i16 = (selection_rates.2 as f32 * population_modifier) as i16;
+        let mut random_selection: i16 =
+            (selection_rates.0 as f32 * population_modifier) as i16;
+        let tournament_selection: i16 =
+            (selection_rates.1 as f32 * population_modifier) as i16;
+        let generate_new_selection: i16 =
+            (selection_rates.2 as f32 * population_modifier) as i16;
         let mut selection_num: i16 =
             random_selection + tournament_selection + generate_new_selection;
 
@@ -52,10 +60,12 @@ impl Genetics {
             }
         }
 
-        selection_num = random_selection + tournament_selection + generate_new_selection;
+        selection_num =
+            random_selection + tournament_selection + generate_new_selection;
         assert!(selection_num == self.population.len() as i16);
 
-        let mut new_pop: Vec<Chromosome> = Vec::with_capacity(self.population.capacity());
+        let mut new_pop: Vec<Chromosome> =
+            Vec::with_capacity(self.population.capacity());
 
         for _ in 0..random_selection {
             let i = rand::thread_rng().gen_range(0..self.population.len());
@@ -67,11 +77,11 @@ impl Genetics {
             tourney_size = 2;
         }
         for _ in 0..tournament_selection {
-            let initial_index =
+            let initial =
                 rand::thread_rng().gen_range(0..self.population.len() - tourney_size);
-            let tourney_pop = self.population[initial_index..initial_index + tourney_size].to_vec();
+            let tourney_pop = self.population[initial..initial + tourney_size].to_vec();
 
-            let mut winner_index = initial_index;
+            let mut winner_index = initial;
             let max_slice_index: Option<usize> = tourney_pop
                 .iter()
                 .enumerate()
@@ -94,20 +104,23 @@ impl Genetics {
             new_pop.insert(i, Chromosome::new());
         }
 
+        assert_eq!(selection_num, new_pop.len() as i16);
         self.population = new_pop;
     }
 
     pub fn crossover(&mut self, crossover_rate: &i8) -> () {
-        for _ in 0..((*crossover_rate as f32 / 100.0) * self.population.len() as f32).round() as i16
+        for _ in 0..((*crossover_rate as f32 / 100.0) * self.population.len() as f32)
+            .round() as i16
         {
             let crossover_a = rand::thread_rng().gen_range(0..self.population.len());
-            let mut crossover_b = rand::thread_rng().gen_range(0..self.population.len());
+            let mut crossover_b =
+                rand::thread_rng().gen_range(0..self.population.len());
             while crossover_a == crossover_b {
                 crossover_b = rand::thread_rng().gen_range(0..self.population.len());
             }
 
-            let slice_size =
-                rand::thread_rng().gen_range(1..self.population[crossover_a].genes.len() / 2);
+            let slice_size = rand::thread_rng()
+                .gen_range(1..self.population[crossover_a].genes.len() / 2);
             let starting_index_a = rand::thread_rng()
                 .gen_range(0..self.population[crossover_a].genes.len() - slice_size);
             let starting_index_b = rand::thread_rng()
@@ -116,19 +129,22 @@ impl Genetics {
             let mut swap_slice = self.population[crossover_b].genes
                 [starting_index_b..starting_index_b + slice_size]
                 .to_vec();
-            self.population[crossover_a].genes[starting_index_a..starting_index_a + slice_size]
+            self.population[crossover_a].genes
+                [starting_index_a..starting_index_a + slice_size]
                 .swap_with_slice(&mut swap_slice);
-            self.population[crossover_b].genes[starting_index_b..starting_index_b + slice_size]
+            self.population[crossover_b].genes
+                [starting_index_b..starting_index_b + slice_size]
                 .swap_with_slice(&mut swap_slice);
         }
     }
 
     pub fn mutation(&mut self, mutation_rate: &(i8, i8)) -> () {
-        for _ in 0..((mutation_rate.0 as f32 / 100.0) * self.population.len() as f32).round() as i16
+        for _ in 0..((mutation_rate.0 as f32 / 100.0) * self.population.len() as f32)
+            .round() as i16
         {
             let m = rand::thread_rng().gen_range(0..self.population.len());
-            for _ in
-                0..((mutation_rate.1 as f32 / 100.0) * self.population[m].genes.len() as f32) as i8
+            for _ in 0..((mutation_rate.1 as f32 / 100.0)
+                * self.population[m].genes.len() as f32) as i8
             {
                 let new_weight_index =
                     rand::thread_rng().gen_range(0..self.population[m].genes.len());
